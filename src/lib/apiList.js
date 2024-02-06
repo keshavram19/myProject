580f5ab058aa55aaad644cb0049041c53a85d1cb
Vishnu7677 <128765651+Vishnu7677@users.noreply.github.com>
2024-02-06 22:02:23 +0530

    06/02/vishnu

--- src/lib/apiList.js
+++ src/lib/apiList.js
@@ -18,9 +18,6 @@ const apiList ={
     createVerificationCode: `${server}/api/generate-otp`,
     authenticateOTP: `${server}/api/validate-otp`,
 
-
-    customerAccountDetails: `${server}/api/userDetails/`,
-    userAuthentication: `${server}/api/otp-send`,
     userAuthVerification: `${server}/api/verify-otp`,
     form60OTPSend:`${server}/form60Userdetails/send-otp`,
     form60OtpVerification:`${server}/form60Userdetails/verify-otp`,
