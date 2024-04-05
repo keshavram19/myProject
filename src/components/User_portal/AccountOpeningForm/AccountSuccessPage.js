4b1d30a91f2649494a5bcc4d7e844b3573a53ec4
borigamsairam21 <sairamborigam21@gmail.com>
2024-04-05 15:39:54 +0530

    05/04/sai

--- src/components/User_portal/AccountOpeningForm/AccountSuccessPage.js
+++ src/components/User_portal/AccountOpeningForm/AccountSuccessPage.js
@@ -3,7 +3,12 @@ import sdf from './images/success.gif'
 import { Link, } from 'react-router-dom';
 
 const AccountSuccessPage = () => {
-
+  // 
+  const [needDebitCard, setNeedDebitCard] = useState(false);
+  const handleDebitCardOption = (value) => {
+    setNeedDebitCard(value);
+  };
+  // 
 
   const handleOkButtonClick = () => {
     window.location.href = 'http://localhost:3000'; 
@@ -14,6 +19,36 @@ const AccountSuccessPage = () => {
       <img src={sdf}/><br/><br/><br/>
       
       <h1 className='account_successful_heading'>Account Created Successfully</h1><br/><br/><br/>
+      {/*  */}
+      <div>
+        <label>
+          Need Debit Card:
+          <div>
+          {/* <button className={needDebitCard ? 'selected' : ''} onClick={() => handleDebitCardOption(true)}>Yes</button>
+          <button className={!needDebitCard ? 'selected' : ''
+        } onClick={() => handleDebitCardOption(false)}>No</button>
+         */}
+          <input
+              type='checkbox'
+              id='yesCheckbox'
+              checked={needDebitCard}
+              onChange={() => handleDebitCardOption(true)}
+            />
+            <label htmlFor='yesCheckbox' className={'ml-2' + (needDebitCard ? 'selected' : '')}>Yes</label>
+            <input
+            className='ml-3'
+              type='checkbox' 
+              id='noCheckbox'
+              checked={!needDebitCard}
+              onChange={() => handleDebitCardOption(false)}
+            />
+            <label htmlFor='noCheckbox' className={'ml-2' + (!needDebitCard ? 'selected' : '')}>No</label>
+          
+        </div>
+        </label>
+      </div>
+      <br />
+      {/*  */}
       <Link className='account_successful_button' onClick={handleOkButtonClick}>OK</Link>
     </div>
   )
