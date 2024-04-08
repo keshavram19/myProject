81c65ab92af184577fa11aa804659538faabe180
borigamsairam21 <sairamborigam21@gmail.com>
2024-04-08 13:16:43 +0530

    08/04/sai

--- src/components/User_portal/Netbanking/Account-Summary/view-summary/viewsummary.js
+++ src/components/User_portal/Netbanking/Account-Summary/view-summary/viewsummary.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-
+ 
 import "./viewsummary.css";
 import creditcardimg from "../../../../../Images/credit_card.svg";
 import OverviewSidebar from '../../Sidebar/OverViewSidebar'
